1592ecf6e47cee0c8a62d1ad5323e068248ecd4a
Lucas-Caronni <lcaronni@udesa.edu.ar>
2024-05-10 13:01:03 -0300

    practica 1 hasta el 4to ejercicio

---
+++ js/ejercicio1.js
@@ -0,0 +1,27 @@
+function rectangulo (base,altura){
+    return base * altura;
+}
+console.log (rectangulo(5,10))
+
+// segundo ejercicio
+
+function triangulo (base,altura){
+    return base * altura / 2;
+}
+console.log (triangulo(5,10))
+
+// tercer ejercicio
+
+function largoDelArray (array){
+    return array.length
+}
+const miArray = [1,2,3,4,5]
+console.log (largoDelArray(miArray));
+
+// cuarto ejercicio
+
+function cantidadDeLetras (palabra){
+    return palabra.length
+}
+const miPalabra = "gallardo"
+console.log (cantidadDeLetras(miPalabra));
