1b127152f103c82e56655a9c04d3339201ed5c29
Lucas-Caronni <lcaronni@udesa.edu.ar>
2024-05-17 11:09:01 -0300

    Hasta el ejercicio 2e

---
+++ Clase 9/js/ejercicio4.js
@@ -0,0 +1,19 @@
+// ejercicio 1 Precalentando
+
+let mensaje = "vas a salir de tu casa?"
+
+let resultado = confirm(mensaje)
+
+if(resultado == true) {
+    alert("chequea el pronostico del tiempo antes de salir")
+
+} else {
+    alert("Que bueno que te quedas. Va a llover mucho hoy")
+}
+
+
+
+
+
+
+
---
+++ Clase 9/js/ejercicio5.js
@@ -0,0 +1,24 @@
+//Ejercicio 2b
+
+let nombre = prompt("Por favor, ingresa tu nombre:");
+
+console.log(nombre);
+
+let edad = prompt("Por favor, ingresa tu edad");
+
+console.log(edad)
+
+let mensaje = "Te gustan los deportes?"
+
+let fanDeportes = confirm(mensaje)
+
+console.log(mensaje) 
+    alert(`Muchas gracias ${nombre}`)
+
+
+
+
+
+
+
+
